a413e632f4f57b4b78140cd341e3ed8cea0b28e2
Joel Kustka <joel.kustka@gmail.com>
2016-12-08 21:40:30 -0800

    Fix the build scripts to surface TypeScript compilation errors

--- build-model.sh
+++ build-model.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 tsc -t es6 -m commonjs model.ts
+if [[ $? -ne 0 ]]; then
+  exit $?
+fi
 node -e 'require("./model.js").buildModel()'
 tsc
+if [[ $? -ne 0 ]]; then
+  exit $?
+fi
 node -e 'require("./index.js").buildModel()'
--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ STAGE_DIR=$BUILD_DIR/stage
 PACKAGE=../alexa-sfmta-llambda.zip
 
 tsc
+
+if [[ $? -ne 0 ]]; then
+  exit $?
+fi
+
 mkdir -p $STAGE_DIR
 cp package.json $STAGE_DIR
 cp ./*.js $STAGE_DIR
